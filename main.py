30a60d2aa7248a886910cf44682d0ac86fdb7e73
Tristan <ricemaking69@gmail.com>
2025-07-26 10:26:12 -0500

    test

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 APP_ID = int(os.getenv("APP_ID"))
 
+# TEST
+TOKEN = "MTM3NjY0MDExMjc5NjEwNjk4Mw.GDVd8A.2SMN6rsrqiafyFtn0BHLqUX-fCyNPhYv35KNVE"
+APP_ID = "1376640112796106983"
+
 # Setup Discord bot
 intents: Intents = Intents.default()
 intents.message_content = True
